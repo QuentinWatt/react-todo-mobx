95263619368878d29a330d024c5a21babb74480e
Quentin <quentin@Quentins-MBP.fritz.box>
2022-11-14 21:03:57 +0100

    mobile styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
       <div className="bg-gray-100 min-h-screen">
         <Header />
-        <div className="container mx-auto">
+        <div className="container mx-auto px-3">
           <TodoForm />
           <TodoList />
         </div>
--- src/Components/TodoForm.tsx
+++ src/Components/TodoForm.tsx
@@ -1,18 +1,26 @@
 function TodoForm() {
+    const addTodo = (event: any) => {
+      event.preventDefault()
+      console.log(event)
+    }
+
     return (
         <form
+          onSubmit={addTodo}
           className="mt-3 bg-white rounded-lg border p-3"
         >
           <h3 className="font-bold text-2xl mb-3">
             Add a task
           </h3>
-          <div className="flex items-center">
+          <div className="flex flex-col md:flex-row">
             <input
               type="text"
-              className="border py-2 px-3 rounded w-1/2"
+              className="border py-2 px-3 rounded w-full md:w-1/2 md:mr-3"
               placeholder="Something to do."
             />
-            <button className="w-32 bg-primary-500 rounded text-white py-2 px-3 ml-3">
+            <button
+              className="mt-3 md:mt-0 w-full md:w-32 bg-primary-500 rounded text-white py-2 px-3"
+            >
               Add
             </button>
           </div>
