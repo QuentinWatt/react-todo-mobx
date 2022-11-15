1793993be69ff9d42562ac033b9744e1822863a9
Quentin <quentin@qcasts.com>
2022-11-15 12:10:59 +0100

    Todo form validation (#2)
    * Todo form validation
    
    Co-authored-by: Quentin <quentin@Quentins-MBP.fritz.box>

--- src/Components/TodoForm.tsx
+++ src/Components/TodoForm.tsx
@@ -1,39 +1,63 @@
-import { useState } from "react";
+import React from "react";
+import {FormEvent, useState} from "react";
 import todoStore from "../Store/TodoStore";
 
 function TodoForm() {
-    const [ todoTitle, setTodoTitle ] = useState('')
+  const [ todoTitle, setTodoTitle ] = useState('')
+  const [ errors, setErrors ] = useState<Array<string>>([])
 
-    const addTodo = (event: any) => {
-      event.preventDefault()
+  const update = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoTitle(event.target.value)
+    if(errors.length){
+      setErrors([])
+    }
+  }
+
+  const addTodo = (event: FormEvent) => {
+    setErrors([])
+    event.preventDefault()
+    if(todoTitle.length){
       todoStore.makeTodo(todoTitle)
       setTodoTitle('')
     }
+    else {
+      setErrors(['Enter a value'])
+    }
+  }
 
-    return (
-      <form
-        onSubmit={ addTodo }
-        className="mt-3 bg-white rounded-lg border p-3"
-      >
-        <h3 className="font-bold text-2xl mb-3">
-          Add a task
-        </h3>
-        <div className="flex flex-col md:flex-row">
+  return (
+    <form
+      onSubmit={ addTodo }
+      className="mt-3 bg-white rounded-lg border p-3"
+    >
+      <h3 className="font-bold text-2xl mb-3">
+        Add a task
+      </h3>
+      <div className="flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2 md:mr-3">
           <input
             type="text"
-            className="border py-2 px-3 rounded w-full md:w-1/2 md:mr-3"
-            onChange={event => setTodoTitle(event.target.value)}
+            className="border py-2 px-3 rounded block w-full"
+            onChange={event => update(event)}
             value={todoTitle}
             placeholder="Something to do."
           />
+          { errors.map((error) => (
+            <div className="text-red-500 text-sm">
+              { error }
+            </div>
+          )) }
+        </div>
+        <div>
           <button
-            className="mt-3 md:mt-0 w-full md:w-32 bg-primary-500 rounded text-white py-2 px-3"
+            className="mt-3 md:mt-0 w-full md:w-32 bg-primary-500 hover:bg-primary-600 rounded text-white py-2 px-3"
           >
             Add
           </button>
         </div>
-      </form>
-    )
+      </div>
+    </form>
+  )
 }
 
 export default TodoForm
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,15 @@ module.exports = {
       colors: {
         primary: {
           500: '#2274A5',
+          600: '#1a6591',
         },
         secondary: {
           500: '#e83f6f',
+          600: '#c93660',
         },
         tertiary: {
           500: '#32936f',
+          600: '#237557',
         },
         quaternary: {
           500: '#ffbf00',
